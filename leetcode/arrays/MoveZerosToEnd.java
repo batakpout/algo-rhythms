e896ec73400d5db89d9dead0316d40485a048928
aamir <16462830+batakpout@users.noreply.github.com>
2025-06-28 17:11:42 +0530

    Add files via upload

---
+++ leetcode/arrays/MoveZerosToEnd.java
@@ -0,0 +1,68 @@
+package leetcode.arrays;
+
+import static utils.ArrayUtils.swap;
+
+/**
+ * 283. Move Zeroes
+ * <p>
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * Note that you must do this in-place without making a copy of the array.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [0]
+ * Output: [0]
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
+ */
+public class MoveZerosToEnd {
+
+    //O(N*N)
+    public void bruteForce(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < n; j++) {
+                    if (nums[j] != 0) {
+                        swap(nums, i, j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    //O(N)
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
