796c2435c0cc7f37af388fcbe84c94ee79ec196e
aamir <16462830+batakpout@users.noreply.github.com>
2025-07-04 20:30:43 +0530

    Create ListNode.java

---
+++ leetcode/linked_list/ListNode.java
@@ -0,0 +1,19 @@
+package leetcode.linkedlist;
+
+public class ListNode {
+
+    int data;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int data) {
+        this.data = data;
+    }
+
+    ListNode(int data, ListNode next) {
+        this.data = data;
+        this.next = next;
+    }
+}
