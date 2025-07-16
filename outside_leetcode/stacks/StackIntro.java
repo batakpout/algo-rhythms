387cb4df5d23c63ccdf1ad7fc6bc1167c1f1b293
aamir <16462830+batakpout@users.noreply.github.com>
2025-07-16 12:49:26 +0530

    Create StackIntro.java

---
+++ outside_leetcode/stacks/StackIntro.java
@@ -0,0 +1,17 @@
+package outside_leetcode.stacks;
+
+/* LIFO
+ underflow happens when peek or pop called on empty stack
+ overflow happens when push called on full stack. [hardly happens because of dynamic sized collection under the hood or linked list]
+ Some real world applications:
+ 1. Function calls
+ 2. balanced parenthesis by compilers
+ 3. Reversing items
+ 4. Infix to Postfix/Prefix
+ 5. Evaluation of Postfix/Prefix
+ 6. Stock span problem and its variants
+ 7. undo/redo or forward/backward in editor and browser.
+ */
+public class Stacks {
+
+}
